af34bd8d5aa365bb9329266f9c238d2d61545756
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-16 18:54:48 +1300

    Add tblIssueModel

---
+++ AucklandTalyer/Models/tblIssue.cs
@@ -0,0 +1,14 @@
+﻿namespace AucklandTalyer.Models
+{
+    public class tblIssue
+    {
+        public int IssueId { get; set; }
+        public string IssueName { get; set; }
+        public string IssueDescription { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string AddedBy { get; set; }
+
+        public int CustomerId { get; set; }
+
+    }
+}
