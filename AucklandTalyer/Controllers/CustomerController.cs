e30d9a823c101d504b2525b5a0a2191e39e33f82
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-16 19:48:41 +1300

    Finish Customer Index

--- AucklandTalyer/Controllers/CustomerController.cs
+++ AucklandTalyer/Controllers/CustomerController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AucklandTalyer.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AucklandTalyer.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly ICustomerRepository _customerRepository;
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;   
+        }
         public IActionResult Index()
         {
-            return View();
+            var customerList = _customerRepository.GetCustomer();
+            return View(customerList);
         }
     }
 }
--- AucklandTalyer/Program.cs
+++ AucklandTalyer/Program.cs
@@ -1,4 +1,5 @@
 using AucklandTalyer.Data;
+using AucklandTalyer.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
