f5a33c2aaf0dcf27784d0cc3512e9ec98be95eaa
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-16 19:06:58 +1300

    Add dbContext

---
+++ AucklandTalyer/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AucklandTalyer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AucklandTalyer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<tblCustomer> tblCustomer { get; set; }
+        public DbSet<tblIssue> tblIssue { get; set; }
+    }
+}
--- AucklandTalyer/Models/tblIssue.cs
+++ AucklandTalyer/Models/tblIssue.cs
@@ -1,10 +1,14 @@
-﻿namespace AucklandTalyer.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AucklandTalyer.Models
 {
     public class tblIssue
     {
+        [Key]
         public int IssueId { get; set; }
         public string IssueName { get; set; }
         public string IssueDescription { get; set; }
+        public float? TotalPrice { get; set; }
         public DateTime DateAdded { get; set; }
         public string AddedBy { get; set; }
 
